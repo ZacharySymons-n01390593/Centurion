985ae62a21f0cada08c790bcbb63ef72bb4d7207
andres <n01359071@humbermail.ca>
2021-10-11 18:47:54 -0400

    added image to CO2 tab and modified UI

--- Centurion/src/main/java/home/control/centurion/LightControl/LightControlFrag.java
+++ Centurion/src/main/java/home/control/centurion/LightControl/LightControlFrag.java
@@ -1,3 +1,4 @@
+//Andres Vargas(N01359071), Ibrahim Abdiaziz(N01394807), Zachary Symons(N01390593), Jonathan Alexandris (N01352690).
 package home.control.centurion.LightControl;
 
 import android.os.Bundle;

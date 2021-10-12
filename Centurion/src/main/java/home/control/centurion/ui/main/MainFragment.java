9ab7b98d60008a9bcc06e85c2398b22d923431ab
JonathanAlexandris2690 <n01352690@humbermail.ca>
2021-10-12 14:42:57 -0400

    added portait lock and dark mode to settings screen

--- Centurion/src/main/java/home/control/centurion/ui/main/MainFragment.java
+++ Centurion/src/main/java/home/control/centurion/ui/main/MainFragment.java
@@ -1,22 +1,27 @@
 //Andres Vargas(N01359071), Ibrahim Abdiaziz(N01394807), Zachary Symons(N01390593), Jonathan Alexandris (N01352690).
 package home.control.centurion.ui.main;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import home.control.centurion.R;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
-
+    int i = 0;
+    int j = 0;
     public static MainFragment newInstance() {
         return new MainFragment();
     }
@@ -26,9 +31,56 @@ public class MainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.main_fragment, container, false);
+        Button btnToggleDark = root.findViewById(R.id.btnDarkModeToggle);
+        Button btnTogglePortraitLock = root.findViewById(R.id.btnTooglePortraitLock);
+
+
+        btnToggleDark.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if (i == 0) {
+                            AppCompatDelegate
+                                    .setDefaultNightMode(
+                                            AppCompatDelegate
+                                                    .MODE_NIGHT_YES);
+
+                            i=1;
+
+                        }else if (i != 0) {
+                            AppCompatDelegate
+                                    .setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                            i=0;
+                        }
+                    }
+                });
+
+        btnTogglePortraitLock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
 
         return root;
     }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+
+        if(isVisibleToUser) {
+            Activity a = getActivity();
+            if(a != null) a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            if(a != null) a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+        }
+    }
+
+
 
 
 

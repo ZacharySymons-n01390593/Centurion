fb895bd0ae93dbd5d50e9efe160f79bcf8d03133
Zachary Symons <zacharysymons@rogers.com>
2021-12-07 18:43:43 -0500

    changes on functionality of LockFrag.java

--- Centurion/src/main/java/home/control/centurion/Lock/LockFrag.java
+++ Centurion/src/main/java/home/control/centurion/Lock/LockFrag.java
@@ -76,7 +76,7 @@ private DatabaseReference databaseReference;
                     //update text fields for user.
                 output.setText(reading);
                 output2.setText(reading2);
-                output3.setText(R.string.greet + reading3);
+                output3.setText(reading3);
             }
 
             @Override

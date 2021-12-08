7307497bf5d1213f450b369121dd3e6c35ec9aa2
Zachary Symons <zacharysymons@rogers.com>
2021-12-07 20:32:34 -0500

    timepicker

---
+++ Centurion/src/main/java/home/control/centurion/LightControl/TimePickFrag.java
@@ -0,0 +1,27 @@
+package home.control.centurion.LightControl;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class TimePickFrag extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getContext(),hour,minute,DateFormat.is24HourFormat(getActivity()));
+
+}
+
+}
